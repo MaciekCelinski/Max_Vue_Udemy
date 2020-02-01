dc29af8ab6e826c52d2273a6b7f229b28d7f05f5
MaciekCelinski <mac.celinski@gmail.com>
2020-02-01 17:43:34 +0100

    Lecture 127

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<app-quote>
+					<h2 slot="title">{{ quoteTitle }}</h2>
+					<p slot="content">A wonderfull Life</p>
+				</app-quote>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+    import Quote from "./components/Quote";
 
-
-export default {
-
-};
+    export default {
+        data(){
+            return {
+                quoteTitle: 'The Qoute'
+            }
+        },
+        components: {
+            appQuote: Quote
+        }
+    };
 </script>
 
 <style>
