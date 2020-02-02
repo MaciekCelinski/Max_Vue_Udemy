8ae5e70d13797a3b1c7129780c0f86c679b65326
MaciekCelinski <mac.celinski@gmail.com>
2020-02-02 23:23:15 +0100

    unfinished start

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="row">
+		<app-quote v-for="quote in quotes" :key="quote">
+			{{ quote }}
+		</app-quote>
+	</div>
+</template>
+
+<script>
+    import Quote from './Quote.vue'
+
+    export default {
+        name: "QuoteGrid",
+        props: ['quotes'],
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
