49f389ab2243bd5f2b9ab064f8e714147bdec6e6
MaciekCelinski <mac.celinski@gmail.com>
2020-02-06 22:29:07 +0100

    Lecture_156

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,138 @@
 <template>
-    <div class="container">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <h1>File a Complaint</h1>
-                    <hr />
-                    <div class="form-group">
-                        <label for="email">Mail</label>
-                        <input type="text" id="email" class="form-control" />
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" class="form-control" />
-                    </div>
-                    <div class="form-group">
-                        <label for="age">Age</label>
-                        <input type="number" id="age" class="form-control" />
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="message">Message</label>
-                    <br />
-                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-                    <textarea id="message" rows="5" class="form-control"></textarea>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <div class="form-group">
-                        <label for="sendmail">
-                            <input type="checkbox" id="sendmail" value="SendMail" /> Send Mail
-                        </label>
-                        <label for="sendInfomail">
-                            <input type="checkbox" id="sendInfomail" value="SendInfoMail" /> Send Infomail
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="male">
-                        <input type="radio" id="male" value="Male" /> Male
-                    </label>
-                    <label for="female">
-                        <input type="radio" id="female" value="Female" /> Female
-                    </label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
-                    <label for="priority">Priority</label>
-                    <select id="priority" class="form-control">
-                        <option></option>
-                    </select>
-                </div>
-            </div>
-            <hr />
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <button class="btn btn-primary">Submit!</button>
-                </div>
-            </div>
-        </form>
-        <hr />
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Mail:</p>
-                        <p>Password:</p>
-                        <p>Age:</p>
-                        <p>Message:</p>
-                        <p>
-                            <strong>Send Mail?</strong>
-                        </p>
-                        <ul>
-                            <li></li>
-                        </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
-                        <p>Switched:</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<form>
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+					<h1>File a Complaint</h1>
+					<hr/>
+					<div class="form-group">
+						<label for="email">Mail</label>
+						<input type="text" v-model.lazy="user.email" id="email" class="form-control"/>
+					</div>
+					<div class="form-group">
+						<label for="password">Password</label>
+						<input type="password" id="password" v-model.lazy="user.password" class="form-control"/>
+					</div>
+					<div class="form-group">
+						<label for="age">Age</label>
+						<input type="number" id="age" v-model.lazy="user.age" class="form-control"/>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+					<label for="message">Message</label>
+					<br/>
+					<!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+					<textarea id="message" v-model="message" rows="5" class="form-control"></textarea>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+					<div class="form-group">
+						<label for="sendmail">
+							<input
+								type="checkbox"
+								id="sendmail"
+								value="SendMail"
+								v-model="sendMail"
+							> Send Mail
+						</label>
+						<label for="sendInfomail">
+							<input
+								type="checkbox"
+								id="sendInfomail"
+								value="SendInfoMail"
+								v-model="sendMail"
+							> Send Infomail
+						</label>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+					<label for="male">
+						<input
+							type="radio"
+							id="male"
+							value="Male"
+                        v-model="gender"> Male
+					</label>
+					<label for="female">
+						<input type="radio"
+						       id="female"
+						       value="Female"
+                               v-model="gender"> Female
+					</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+					<label for="priority">Priority</label>
+					<select id="priority" class="form-control" v-model="selectedPriority">
+						<option
+                            v-for="priority in priorities"
+                            :selected="priority == 'Medium'"
+                        >{{priority}}
+                        </option>
+					</select>
+				</div>
+			</div>
+			<hr/>
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+					<button class="btn btn-primary">Submit!</button>
+				</div>
+			</div>
+		</form>
+		<hr/>
+		<div class="row">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>Your Data</h4>
+					</div>
+					<div class="panel-body">
+						<p>Mail: {{user.email}}</p>
+						<p>Password: {{user.password}}</p>
+						<p>Age: {{user.age}}</p>
+						<!--  style="white-space: pre" is a CSS that will allow us to divide text to different lines  -->
+						<p style="white-space: pre">Message: {{message}}</p>
+						<p>
+							<strong>Send Mail?</strong>
+						</p>
+						<ul>
+							<li v-for="item in sendMail">{{item}}</li>
+						</ul>
+						<p>Gender: {{gender}}</p>
+						<p>Priority: {{selectedPriority}}</p>
+						<p>Switched:</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {};
+    export default {
+        data() {
+            return {
+                user: {
+                    email: '',
+                    password: "",
+                    age: {
+                        type: Number
+                    }
+                },
+                message: "Hello",
+                sendMail: [],
+                gender: "Male",
+                selectedPriority: 'High',
+                priorities: ['High', "Medium","Low"]
+            }
+        }
+    };
 </script>
 
 <style>
