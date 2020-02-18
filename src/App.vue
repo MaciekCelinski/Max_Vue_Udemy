11454e657babffcb6f760e8f44f2dae73339c9a0
MaciekCelinski <mac.celinski@gmail.com>
2020-02-18 20:21:30 +0100

    slide

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,14 @@
 				<br>
 				<br>
 				<transition name="fade">
-				<div class="alert alert-info" v-if="showInfo">
-					This is some info
-				</div>
+					<div class="alert alert-info" v-if="showInfo">
+						This is some info
+					</div>
+				</transition>
+				<transition name="slide">
+					<div class="alert alert-info" v-if="showInfo">
+						This is some info
+					</div>
 				</transition>
 			</div>
 		</div>
@@ -33,14 +38,51 @@
 	.fade-enter {
 		opacity: 0
 	}
-	.fade-enter-active{
-		transition: opacity 1s;
+
+	.fade-enter-active {
+		transition: opacity 0.5s;
 	}
-	.fade-leave{
+
+	.fade-leave {
 		/*opacity: 1*/
 	}
-	.fade-leave-active{
-		transition: opacity 1s;
+
+	.fade-leave-active {
+		transition: opacity 0.5s;
 		opacity: 0;
+	}
+
+	.slide-enter {
+		/*transform: translateY(20px);*/
+	}
+
+	.slide-enter-active {
+		animation: slide-in 0.5s ease-out forwards;
+	}
+
+	.slide-leave {
+
+	}
+
+	.slide-leave-active {
+		animation: slide-out 0.5s ease-out forwards;
+	}
+
+	@keyframes slide-in {
+		from {
+			transform: translateY(20px);
+		}
+		to {
+			transform: translateY(0);
+		}
+	}
+
+	@keyframes slide-out {
+		from {
+			transform: translateY(0);
+		}
+		to {
+			transform: translateY(20px);
+		}
 	}
 </style>
