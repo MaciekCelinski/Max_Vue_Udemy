33058d34aedf635fbf5a7ec9d608a9e8fc56a75b
MaciekCelinski <mac.celinski@gmail.com>
2020-02-26 21:10:09 +0100

    Lecture_210

--- src/App.vue
+++ src/App.vue
@@ -74,13 +74,16 @@
 				<br/>
 				<br/>
 				<ul class="list-group">
-					<li
-						class="list-group-item"
-						v-for="number in numbers"
-						@click="deleteItem(index)"
-						style="cursor: pointer"
-					>{{number}}
-					</li>
+					<transition-group name="slide">
+						<li
+							class="list-group-item"
+							v-for="(number,index) in numbers"
+							@click="deleteItem(index)"
+							style="cursor: pointer"
+							:key="number"
+						>{{number}}
+						</li>
+					</transition-group>
 				</ul>
 			</div>
 		</div>
@@ -202,7 +205,12 @@
 	.slide-leave-active {
 		animation: slide-out 1s ease-out forwards;
 		transition: opacity 1s;
-		opacity: 0
+		opacity: 0;
+		position: absolute;
+	}
+
+	.slide-move {
+		transition: transform 1s;
 	}
 
 	@keyframes slide-in {
