58f3cc66b39237f371a35de534967ea10633a218
MaciekCelinski <mac.celinski@gmail.com>
2020-03-19 17:53:59 +0100

    Lecture_228

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vue-http-ce999.firebaseio.com/vuejs-http/'
+Vue.http.interceptors.push((request, next)=>{
+  console.log(request)
+  if(request.method === 'POST') {
+    request.method = 'PUT'
+  }
+  next(response => {
+    response.json = () => {
+      return {
+        messages: response.body
+      }
+    }
+  });
+})
 
 new Vue({
   el: '#app',
