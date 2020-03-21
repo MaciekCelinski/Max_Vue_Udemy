f40b1da0a051510735f48045060573f458bc2b9a
MaciekCelinski <mac.celinski@gmail.com>
2020-03-21 14:23:23 +0100

    START

--- src/main.js
+++ src/main.js
@@ -1,25 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueResource from 'vue-resource'
-
-Vue.use(VueResource);
-
-Vue.http.options.root = 'https://vue-http-ce999.firebaseio.com/vuejs-http/'
-Vue.http.interceptors.push((request, next) => {
-    console.log(request)
-    if (request.method === 'POST') {
-        request.method = 'PUT'
-    }
-    next(response => {
-        response.json = () => {
-            return {
-                messages: response.body
-            }
-        }
-    });
-})
 
 new Vue({
-    el: '#app',
-    render: h => h(App)
+  el: '#app',
+  render: h => h(App)
 })
