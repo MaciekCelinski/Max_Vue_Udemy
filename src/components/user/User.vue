517dcdbf9c37b653342974b5e2c8f9c0ff7e085b
MaciekCelinski <mac.celinski@gmail.com>
2020-03-22 11:26:51 +0100

    Lecture_242

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,30 @@
 <template>
-    <h1>The User Page</h1>
-
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <p>Loaded ID: {{id}}</p>
+        <button @click="navigateHome" class="btn btn-primary">Go Home</button>
+    </div>
 </template>
+
+<script>
+    export default {
+        // data(){
+        //     return {
+        //         id: this.$route.params.id
+        //     }
+        // },
+        // watch:{
+        //     '$route'(to, from){
+        //         this.id = to.params.id;
+        //         console.log(to.params)
+        //     }
+        // },
+        props: ['id'],
+        methods: {
+            navigateHome(){
+                this.$router.push('/')
+            }
+        },
+    }
+</script>
