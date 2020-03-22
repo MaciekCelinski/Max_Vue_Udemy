f8910d3d8ca86749e52cb4d845e16c5dc0150098
MaciekCelinski <mac.celinski@gmail.com>
2020-03-22 14:41:45 +0100

    Lecture_247

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,6 +2,11 @@
 	<div>
 		<h3>Some User Details</h3>
 		<p>User loaded has ID: {{ $route.params.id }}</p>
-        <router-link tag="button" :to="'/user/' + $route.params.id + '/edit'" class="btn btn-danger">Edit User Here</router-link>
+        <router-link
+            tag="button"
+            :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100} }"
+            class="btn btn-danger">
+                Edit User Here
+        </router-link>
 	</div>
 </template>
